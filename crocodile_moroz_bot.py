8004cf5f9319fb763ecedc2ea0ed1d69d1c17a1a
const <konst080591otr@mail.ru>
2022-12-30 16:15:00 +0400

    Code processed by beatyfier, fixed identations

--- crocodile_moroz_bot.py
+++ crocodile_moroz_bot.py
@@ -3,44 +3,61 @@ from telebot import types
 import parse_tost
 import random
 
-token = open('security_token.txt', 'r', encoding="utf-8").read().rstrip()
+token = open("security_token.txt", "r", encoding="utf-8").read().rstrip()
 crocBot = telebot.TeleBot(token)
-videoIdFile = open('./resources/video_id.txt', 'r+', encoding="utf-8")
-pictureGuessIdFile = open('./resources/picture_id_guess_place.txt', 'r+', encoding="utf-8")
+videoIdFile = open("./resources/video_id.txt", "r+", encoding="utf-8")
+pictureGuessIdFile = open(
+    "./resources/picture_id_guess_place.txt", "r+", encoding="utf-8"
+)
 VIDEO_MESSAGE_ID = videoIdFile.read().rstrip()
 PICTURE_MESSAGE_ID = pictureGuessIdFile.read().rstrip()
 
 tosts = []
 
-@crocBot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1=types.KeyboardButton("Гиви, тост!")
-    #item2=types.KeyboardButton("Поздравь")
-    markup.add(item1)
-    #markup.add(item2)
-    crocBot.reply_to(message, """Привет, уважаемый крокодил. Я знаю много тостов, просто нажми кнопку.""", reply_markup=markup)
 
-@crocBot.message_handler(commands=['present', 'congratulate', 'поздравь'])
+@crocBot.message_handler(commands=["start", "help"])
+def send_welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Гиви, тост!")
+    # item2=types.KeyboardButton("Поздравь")
+    markup.add(item1)
+    # markup.add(item2)
+    crocBot.reply_to(
+        message,
+        """Привет, уважаемый крокодил. Я знаю много тостов, просто нажми кнопку.""",
+        reply_markup=markup,
+    )
+
+
+@crocBot.message_handler(commands=["present", "congratulate", "поздравь"])
 def send_congrats(message):
     crocBot.reply_to(message, "Поздравляю с Новым годом!")
     global VIDEO_MESSAGE_ID, videoIdFile, PICTURE_MESSAGE_ID, pictureGuessIdFile
-    if VIDEO_MESSAGE_ID == '':
-        video = open('./resources/Крокодил мороз бежит поздравлять.mp4', 'rb')
-        vMsg = crocBot.send_video(message.chat.id, video, supports_streaming = True)
+    if VIDEO_MESSAGE_ID == "":
+        video = open("./resources/Крокодил мороз бежит поздравлять.mp4", "rb")
+        vMsg = crocBot.send_video(message.chat.id, video, supports_streaming=True)
         video.close()
         VIDEO_MESSAGE_ID = vMsg.video.file_id
         videoIdFile.write(VIDEO_MESSAGE_ID)
         videoIdFile.close()
     else:
-        crocBot.send_video(message.chat.id, VIDEO_MESSAGE_ID, supports_streaming = True)
+        crocBot.send_video(message.chat.id, VIDEO_MESSAGE_ID, supports_streaming=True)
 
-    poem = open('./resources/Стих поздравление на Новый год 2023.txt', 'r', encoding="utf-8").read().rstrip()
+    poem = (
+        open(
+            "./resources/Стих поздравление на Новый год 2023.txt", "r", encoding="utf-8"
+        )
+        .read()
+        .rstrip()
+    )
     crocBot.send_message(message.chat.id, poem)
-    crocBot.send_message(message.chat.id, "Угадаем место? Ваши варианты пишите прямо здесь, посмотрим у кого сбылось.")
+    crocBot.send_message(
+        message.chat.id,
+        "Угадаем место? Ваши варианты пишите прямо здесь, посмотрим у кого сбылось.",
+    )
 
-    if PICTURE_MESSAGE_ID == '':
-        pictureGuess = open('./resources/Угадаем место.jpg', 'rb')
+    if PICTURE_MESSAGE_ID == "":
+        pictureGuess = open("./resources/Угадаем место.jpg", "rb")
         picMsg = crocBot.send_photo(message.chat.id, pictureGuess)
         pictureGuess.close()
         PICTURE_MESSAGE_ID = picMsg.photo[0].file_id
@@ -50,25 +67,27 @@ def send_congrats(message):
         crocBot.send_photo(message.chat.id, PICTURE_MESSAGE_ID)
 
 
-@crocBot.message_handler(commands=['tost', 'тост', 'гивитост'])
+@crocBot.message_handler(commands=["tost", "тост", "гивитост"])
 def send_tost(message):
     global tosts
     if len(tosts) == 0:
         tosts = parse_tost.getTosts()
-    print("Тостов осталось " + str( len(tosts) ))
-    randomTostId = random.randint(0, len(tosts)-1)
+    print("Тостов осталось " + str(len(tosts)))
+    randomTostId = random.randint(0, len(tosts) - 1)
     crocBot.reply_to(message, tosts[randomTostId])
     del tosts[randomTostId]
+
 
 @crocBot.message_handler(func=lambda m: True)
 def echo_all(message):
     if message.text == "Гиви, тост!":
         send_tost(message)
 
-    #if message.text == "Поздравь":
+    # if message.text == "Поздравь":
     #    send_congrats(message)
 
-    #crocBot.reply_to(message, message.text)
+    # crocBot.reply_to(message, message.text)
+
 
 crocBot.infinity_polling()
-#crocBot.polling(none_stop=False, interval=1)
+# crocBot.polling(none_stop=False, interval=1)
--- parse_tost.py
+++ parse_tost.py
@@ -4,73 +4,81 @@ import json
 
 # Parse specific website to get tosts
 def parseTosts():
-  urls = []
-  urls.append('https://pozdravok.com/toast/prazdnik/noviy-god/')
-  tosts = []
+    urls = []
+    urls.append("https://pozdravok.com/toast/prazdnik/noviy-god/")
+    tosts = []
 
-  # going through all pages with tosts if there are more than one
-  while True:
-    r = requests.get( urls[-1] )
-    #print( r.text)
-    with open('test.html', 'wb') as outputFile:
-      outputFile.write(r.text.encode('cp1251'))
+    # going through all pages with tosts if there are more than one
+    print("Hello")
+    while True:
+        r = requests.get(urls[-1])
+        # print( r.text)
+        with open("test.html", "wb") as outputFile:
+            outputFile.write(r.text.encode("cp1251"))
 
-    #Example of html
-    #<link rel="next" title="Следующая - тосты на Новый год" href="/toast/prazdnik/noviy-god/2.htm" />
-    tree = html.fromstring(r.text)
-    linkNext = tree.xpath('//link[@rel = "next"]/@href') # link tag with the attribute rel equial "next" - checking if other pages exist
+        # Example of html
+        # <link rel="next" title="Следующая - тосты на Новый год" href="/toast/prazdnik/noviy-god/2.htm" />
+        tree = html.fromstring(r.text)
+        linkNext = tree.xpath(
+            '//link[@rel = "next"]/@href'
+        )  # link tag with the attribute rel equial "next" - checking if other pages exist
 
-    tostOnPage = tree.xpath('//p[@class = "sfst"]/text()') # Get all tost text
-    tostOnPageBegin = tree.xpath('//p[@class = "sfst"]') # Get the beginning of tosts to split the text
-    tostCount = len(tostOnPage)
-    tostIndexes = []
-    index = 0
+        tostOnPage = tree.xpath('//p[@class = "sfst"]/text()')  # Get all tost text
+        tostOnPageBegin = tree.xpath(
+            '//p[@class = "sfst"]'
+        )  # Get the beginning of tosts to split the text
+        tostCount = len(tostOnPage)
+        tostIndexes = []
+        index = 0
 
-    # Find all indexes where the new tost is started
-    for currentTost in tostOnPageBegin:
-      for i in range(index, tostCount):
-        if currentTost.text == tostOnPage[i]:
-          index = i
-          tostIndexes.append(index)
-          ++index
-          break
+        # Find all indexes where the new tost is started
+        for currentTost in tostOnPageBegin:
+            for i in range(index, tostCount):
+                if currentTost.text == tostOnPage[i]:
+                    index = i
+                    tostIndexes.append(index)
+                    ++index
+                    break
 
-    # Combine separate lines into tosts
-    prev = 0
-    indexesLength = len(tostIndexes)
-    for i in range(0, indexesLength):
-      lastIndex = tostIndexes[i+1] if (i+1) != indexesLength else tostCount
-      tost = ""
-      split4Lines = 1
-      for j in range(tostIndexes[i], lastIndex):
-        print(split4Lines)
-        tost = tost + tostOnPage[j] + ('\n\n' if (split4Lines%4 == 0) else '\n')
-        split4Lines = split4Lines + 1
+        # Combine separate lines into tosts
+        prev = 0
+        indexesLength = len(tostIndexes)
+        for i in range(0, indexesLength):
+            lastIndex = tostIndexes[i + 1] if (i + 1) != indexesLength else tostCount
+            tost = ""
+            split4Lines = 1
+            for j in range(tostIndexes[i], lastIndex):
+                print(split4Lines)
+                tost = (
+                    tost + tostOnPage[j] + ("\n\n" if (split4Lines % 4 == 0) else "\n")
+                )
+                split4Lines = split4Lines + 1
 
-      tosts.append( tost )
+            tosts.append(tost)
 
-    # Check if website has another pages with tosts
-    if len(linkNext) > 0:
-      urls.append(urls[0] + linkNext[0].split('/')[-1])
-      print(urls[-1])
-    else:
-        break
+        # Check if website has another pages with tosts
+        if len(linkNext) > 0:
+            urls.append(urls[0] + linkNext[0].split("/")[-1])
+            print(urls[-1])
+        else:
+            break
 
-  return tosts
+    return tosts
+
 
 # Returns tosts from file, parse website if empty
 def getTosts():
-  tosts = []
-  pathName = "./resources/tosts.json"
-  with open(pathName, 'r', encoding='utf-8') as j:
-    try:
-      tosts = json.load(j)
-    except:
-      tosts = []
+    tosts = []
+    pathName = "./resources/tosts.json"
+    with open(pathName, "r", encoding="utf-8") as j:
+        try:
+            tosts = json.load(j)
+        except:
+            tosts = []
 
-  if len(tosts) == 0:
-    tostsId = open(pathName, "w", encoding='utf-8')
-    tosts = parseTosts()
-    json.dump(tosts, tostsId, ensure_ascii=False, indent=4)
+    if len(tosts) == 0:
+        tostsId = open(pathName, "w", encoding="utf-8")
+        tosts = parseTosts()
+        json.dump(tosts, tostsId, ensure_ascii=False, indent=4)
 
-  return tosts
+    return tosts
