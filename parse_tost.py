b20d63a5ab290a6f492ff5496845b9da33f65a99
const <konst080591otr@mail.ru>
2022-12-30 13:11:58 +0400

    Removed commented code, fixed comments to english

--- parse_tost.py
+++ parse_tost.py
@@ -2,21 +2,23 @@ import requests
 from lxml import html
 import json
 
+# Parse specific website to get tosts
 def parseTosts():
   urls = []
   urls.append('https://pozdravok.com/toast/prazdnik/noviy-god/')
   tosts = []
 
+  # going through all pages with tosts if there are more than one
   while True:
     r = requests.get( urls[-1] )
     #print( r.text)
     with open('test.html', 'wb') as outputFile:
       outputFile.write(r.text.encode('cp1251'))
 
+    #Example of html
     #<link rel="next" title="Следующая - тосты на Новый год" href="/toast/prazdnik/noviy-god/2.htm" />
-    #movie_desc = item_lxml.xpath('.//div[@class = "nameRus"]/a/text()')[0]
     tree = html.fromstring(r.text)
-    linkNext = tree.xpath('//link[@rel = "next"]/@href') # link тег с атрибутом rel равным "next" - проверяем есть ли ещё страницы
+    linkNext = tree.xpath('//link[@rel = "next"]/@href') # link tag with the attribute rel equial "next" - checking if other pages exist
 
     tostOnPage = tree.xpath('//p[@class = "sfst"]/text()') # Get all tost text
     tostOnPageBegin = tree.xpath('//p[@class = "sfst"]') # Get the beginning of tosts to split the text
@@ -24,18 +26,16 @@ def parseTosts():
     tostIndexes = []
     index = 0
 
+    # Find all indexes where the new tost is started
     for currentTost in tostOnPageBegin:
       for i in range(index, tostCount):
-        #print(currentTost.text)
-        #print(tostOnPage[i])
         if currentTost.text == tostOnPage[i]:
           index = i
           tostIndexes.append(index)
           ++index
           break
 
-    #print( tostIndexes )
-
+    # Combine separate lines into tosts
     prev = 0
     indexesLength = len(tostIndexes)
     for i in range(0, indexesLength):
@@ -49,9 +49,7 @@ def parseTosts():
 
       tosts.append( tost )
 
-    #for line in tosts:
-      #print(line)
-
+    # Check if website has another pages with tosts
     if len(linkNext) > 0:
       urls.append(urls[0] + linkNext[0].split('/')[-1])
       print(urls[-1])
@@ -60,6 +58,7 @@ def parseTosts():
 
   return tosts
 
+# Returns tosts from file, parse website if empty
 def getTosts():
   tosts = []
   pathName = "./resources/tosts.json"
@@ -68,9 +67,7 @@ def getTosts():
       tosts = json.load(j)
     except:
       tosts = []
-  
-  #print( len(tosts) )
-  
+
   if len(tosts) == 0:
     tostsId = open(pathName, "w", encoding='utf-8')
     tosts = parseTosts()
